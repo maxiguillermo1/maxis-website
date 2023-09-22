8e56dba2c5f583613dca32905d9d18c19a798c1e
maxi <maxi@dhcp-39-63-238.dhcpint.csulb.edu>
2023-09-21 21:01:45 -0700

    bug fix 5

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,7 @@ import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
 import { FaLinkedin } from 'react-icons/fa';
-import thumbYouTube from '../public/images/links/youtube.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+
 import Image from 'next/image'
 
 const ProfileImage = chakra(Image, {
