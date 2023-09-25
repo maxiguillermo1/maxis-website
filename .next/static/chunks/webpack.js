ee735a8f01c6ad1a3074a33b512f770ab87ae0d2
maxi <maxi@maxis-MacBook-Air.local>
2023-09-25 01:54:17 -0700

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d38e745b916b98d"; }
+/******/ 		__webpack_require__.h = function() { return "b159ce9798e5e91e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
